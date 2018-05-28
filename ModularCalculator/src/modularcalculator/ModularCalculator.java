2bb651b179fe0a351c69b91f5fe49953fe3015c0
Julian <jgarcia@ias.com.co>
2018-05-27 20:48:17 -0500

    primero commit

---
+++ ModularCalculator/src/modularcalculator/ModularCalculator.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modularcalculator;
+
+import java.math.BigInteger;
+
+/**
+ *
+ * @author chori
+ */
+public class ModularCalculator {
+     private int zn;
+    private BigInteger a, b;
+
+    public int getZn() {
+        return zn;
+    }
+
+    public void setZn(int zn) {
+        this.zn = zn;
+    }
+
+    public BigInteger getA() {
+        return a;
+    }
+
+    public void setA(BigInteger a) {
+        this.a = a;
+    }
+
+    public BigInteger getB() {
+        return b;
+    }
+
+    public void setB(BigInteger b) {
+        this.b = b;
+    }
+
+    /**
+     * @param args the command line arguments
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }*/
+    
+}
